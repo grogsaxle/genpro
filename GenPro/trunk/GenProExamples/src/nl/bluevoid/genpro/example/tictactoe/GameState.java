7335499da94e099b2a32a14c0bd64fa1978dac33
robbio33 <robbio33@6bc2275a-b187-11de-9c04-c7668166297d>
2009-10-27 12:09:21 +0000

    removed nonworking examples

--- GenPro/trunk/GenProExamples/src/nl/bluevoid/genpro/example/tictactoe/GameState.java
+++
@@ -1,5 +0,0 @@
-package nl.bluevoid.genpro.example.tictactoe;
-
-public enum GameState {
-  YOU_WIN, DRAW, YOU_LOST, PLAY;
-}
--- GenPro/trunk/GenProExamples/src/nl/bluevoid/genpro/example/tictactoe/ItIsADrawException.java
+++
@@ -1,6 +0,0 @@
-package nl.bluevoid.genpro.example.tictactoe;
-
-@SuppressWarnings("serial")
-public class ItIsADrawException extends Exception {
-
-}
